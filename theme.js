6ec150919b81a5564fb9925661eea57ebe22e472
Nacho Caiafa <nachokai@outlook.com>
2021-04-04 01:18:02 -0300

    fix parse currency

--- theme.js
+++ theme.js
@@ -2,7 +2,7 @@ import { extendTheme, theme } from "@chakra-ui/react";
 
 export default extendTheme({
 	colors: {
-		primary: theme.colors["teal"],
+		primary: theme.colors["purple"],
 	},
 	styles: {
 		body: {
--- utils/parseCurrency.js
+++ utils/parseCurrency.js
@@ -1,4 +1,5 @@
 export const parseCurrency = value =>
+	value &&
 	value.toLocaleString("es-AR", {
 		style: "currency",
 		currency: "ARS",
