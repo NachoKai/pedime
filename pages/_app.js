8fe11b0fbcea281feb0867b7a5b7ee8ea23079ae
Nacho Caiafa <nachokai@outlook.com>
2021-04-03 21:52:28 -0300

    seo improvements

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
-import React from "react";
-import "../styles/globals.css";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import { ChakraProvider, Container, Box } from "@chakra-ui/react";
 import theme from "../theme";
+import "../styles/globals.css";
 
 const App = ({ Component, pageProps }) => {
+	const { pathname } = useRouter();
+	const lang = pathname.startsWith("/es") ? "es" : "en";
+
+	useEffect(() => {
+		document.documentElement.lang = lang;
+	}, [lang]);
+
 	return (
 		<ChakraProvider theme={theme}>
 			<Box p={4}>
